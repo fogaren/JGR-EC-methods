e2ef4b764f55bc89e2f052fd6688057f8596a6e5
Kristen Fogaren <fogaren@users.noreply.github.com>
2021-01-04 17:27:37 -0500

    Update wvnum_omvec.m
    updated INPUTS and OUTPUTS

--- wvnum_omvec.m
+++ wvnum_omvec.m
@@ -1,31 +1,41 @@
-% this program solves for wavenumber using the linear
-% dispersion relation and an iterative Newton-Raphson
-% technique; input is h, om, g
-
+% input is h, om, g % What is om? 
 function[k] = wvnum_omvec(h,sigma,g)
+%==========================================================================
+% This function solves for wavenumber (k) using the linear dispersion 
+% relation and an interative Newton-Raphson technique
 
-% initial guess for iteration
+% Developed by C.E. Reimers for the manuscript published 
+% ***INSERT CITATION***
 
-koh=(sigma.^2)*h/g;
-kx=koh.^(.75);
-khold=koh.*((exp(kx)+exp(-kx))./(exp(kx)-exp(-kx))).^(2/3);
+%*** INPUTS:
+% h = 
+% sigma = 
+% g = 
+
+%*** OUTPUT:
+% k = wavenumber 
+%==========================================================================
+
+koh = (sigma.^2)*h/g;
+kx = koh.^(.75);
+khold = koh.*((exp(kx) + exp(-kx))./(exp(kx) - exp(-kx))).^(2/3);
 
 % iterate w/Newton Raph method
 
-for i=1:100
-dkh1=koh-khold.*((exp(khold)-exp(-khold))./(exp(khold)+exp(-khold)));
-dkh2=-(khold./((0.5*(exp(khold)+exp(-khold))).^2))-...
-     ((exp(khold)-exp(-khold))./(exp(khold)+exp(-khold)));
-dkh=-dkh1./dkh2;
-khnew=khold+dkh;
+for i = 1:100
+dkh1 = koh - khold.*((exp(khold) - exp(-khold))./(exp(khold) + exp(-khold)));
+dkh2 = -(khold./((0.5*(exp(khold) + exp(-khold))).^2))-...
+     ((exp(khold) - exp(-khold))./(exp(khold) + exp(-khold)));
+dkh = -dkh1./dkh2;
+khnew = khold + dkh;
 
 % convergence criterion
 
 if abs(dkh./khnew) <= 0.00001
    break
 else 
-   khold=khnew;
+   khold = khnew;
 end
 end
-k=khnew/h;
+k = khnew/h;
 
