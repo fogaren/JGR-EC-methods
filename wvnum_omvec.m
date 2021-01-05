2fb616af9370b76bbca0f4464134dfeb4fd4457c
Kristen Fogaren <fogaren@users.noreply.github.com>
2021-01-05 14:08:53 -0500

    Update wvnum_omvec.m
    updated inputs/outputs

--- wvnum_omvec.m
+++ wvnum_omvec.m
@@ -1,19 +1,18 @@
-% input is h, om, g % What is om? 
 function[k] = wvnum_omvec(h,sigma,g)
 %==========================================================================
 % This function solves for wavenumber (k) using the linear dispersion 
 % relation and an interative Newton-Raphson technique
+% This function is called in pres2H.m
 
-% Developed by C.E. Reimers for the manuscript published 
-% ***INSERT CITATION***
+% Developed by Tuba Ozkan-Haller (2011, unpublished) 
 
-%*** INPUTS:
-% h = 
-% sigma = 
-% g = 
+%*** INPUTS: are defined in pres2H.m 
+% h = mean water depth at the site (m); variable input is acth in pres2H.m
+% sigma = 2*pi*freq; variable input is w in pres2H.m
+% g = gravity (g = 9.81 m/s2)
 
 %*** OUTPUT:
-% k = wavenumber 
+% k = wavenumber (rad/m)(-)?
 %==========================================================================
 
 koh = (sigma.^2)*h/g;
